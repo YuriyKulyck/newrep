e9e2dd7d9cf14847a112c867a52c12cfccdc7c99
YuriyKulyck <141335054+YuriyKulyck@users.noreply.github.com>
2023-10-08 18:38:54 +0300

    GIGAMuhammad

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from PyQt5.QtWidgets import*
 
+import json
+
+try:
+    with open("notes_data.json", "r", encoding = "utf - 8") as file:
+        notes = json.load(file)
+except:
+    notes = {}
+
+
+
+
 app = QApplication([])
 window = QWidget()
 window.resize(900, 700)
@@ -42,5 +53,54 @@ line2.addLayout(lineer2)
 line2.addWidget(Napoleon5)
 window.setLayout(mainline)
 
+def save_note():
+    if algebra.selectedItems():
+        key = algebra.selectedItems()[0].text()
+        notes[key]["текст"] = textplace.toPlainText()
+        with open("notes_data.json", "w", encoding="utf-8") as file:
+            json.dump(notes, file, ensure_ascii=False)
+    else:
+        print("Замітка для збереження не вибрана!")
+
+def show_note():
+    # отримуємо текст із замітки з виділеною назвою та відображаємо її в полі редагування
+    key = algebra.selectedItems()[0].text()
+    print(key)
+    textplace.setText(notes[key]["текст"])
+    geometrydash.clear()
+    geometrydash.addItems(notes[key]["теги"])
+
+def del_note():
+    if algebra.selectedItems():
+        key = algebra.selectedItems()[0].text()
+        notes.pop(key)
+        algebra.clear()
+        geometrydash.clear()
+        textplace.clear()
+        algebra.addItems(notes)
+        with open("notes_data.json", "w", encoding="utf-8") as file:
+            json.dump(notes, file, sort_keys=True, ensure_ascii=False, indent=4)
+
+    else:
+        print("Замітка для вилучення не обрана!")
+def add_note():
+    note_name, ok = QInputDialog.getText(window, "Додати замітку", "Назва замітки: ")
+    if ok and note_name != "":
+        notes[note_name] = {
+            "текст": "",
+            "теги": []
+        }
+        algebra.clear()
+        textplace.clear()
+        algebra.addItems(notes)
+        with open("notes_data.json", "w", encoding = "utf - 8") as file:
+            json.dump(notes, file, ensure_ascii=False, indent=4)
+
+
+Napoleon0.clicked.connect(add_note)
+Napoleon1.clicked.connect(del_note)
+Napoleon2.clicked.connect(save_note)
+algebra.itemClicked.connect(show_note)
+algebra.addItems(notes)
 window.show()
 app.exec()
